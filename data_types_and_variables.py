9da6588768cb4af8eb504acabfeb0454ea9e0040
Corey Baughman <corey.j.baughman@gmail.com>
2023-10-20 16:34:02 -0600

    updated python_practice and added data_types_and_variables.py

---
+++ data_types_and_variables.py
@@ -0,0 +1,12 @@
+# data_types_and_variables.py
+
+print(f'The type of 99.9 is {type(99.9)}')
+print(f"The type of 'False' is {type('False')}")
+print(f'The type of False is {type(False)}')
+print(f"The type of '0' is {type('0')}")
+print(f'The type of 0 is {type(0)}')
+print(f'The type of True is {type(True)}')
+print(f'''
+      The type of 'True' is {type('True')} 
+      and the type of "True" is {type('True')}
+      ''')
